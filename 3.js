6ad1ba00b158654447444a5e98dbf6b4febcb13f
“Brian“ <b.vilchesm@gmail.com>
2026-05-14 20:40:28 -0400

    encontrar mayor y palindromo

--- 3.js
+++ 3.js
@@ -5,3 +5,21 @@
 // [12, 5, 99, 23, 1]
 
 // Encuentra el número mayor sin usar Math.max().
+
+function encontrarMayor() {
+  let arreglo = [12, 5, 99, 23, 1];
+  let mayor = arreglo[0];
+
+  for (let i = 1; i < arreglo.length; i++) {
+    if (arreglo[i] > mayor) {
+      mayor = arreglo[i];
+    }
+  }
+  return mayor;
+}
+console.log(encontrarMayor());
+
+//Como comparo el primero numero del array con los siguientes?
+//Puedo usar un ciclo for para recorrer el arreglo y comparar cada elemento con el número mayor actual.
+//Inicializo la variable "mayor" con el primer elemento del arreglo y luego comparo cada elemento siguiente con "mayor".
+//Si encuentro un número mayor, actualizo la variable "mayor" con ese número. Al final del ciclo, "mayor" contendrá el número más grande del arreglo.
--- 4.js
+++ 4.js
@@ -6,3 +6,20 @@
 
 // reconocer → true
 // perro → false
+
+function esPalindromo(palabra) {
+  let invertida = "";
+
+  for (i = palabra.length - 1; i >= 0; i--) {
+    invertida += palabra[i];
+  }
+  if (invertida === palabra) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(esPalindromo("perro"));
+console.log(esPalindromo("rata"));
+console.log(esPalindromo("ana"));
